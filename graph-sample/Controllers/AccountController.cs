e117d2f4646336762f667876ae58c0478c4f212b
Chris Wang <Chrismiaow@gmail.com>
2018-11-05 13:29:01 +0800

    Add project files.

---
+++ graph-sample/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace graph_sample.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                // Signal OWIN to send an authorization request to Azure
+                Request.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        public ActionResult SignOut()
+        {
+            if (Request.IsAuthenticated)
+            {
+                Request.GetOwinContext().Authentication.SignOut(
+                    CookieAuthenticationDefaults.AuthenticationType);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ graph-sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="graph_sample.MvcApplication" Language="C#" %>
